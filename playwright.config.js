9fd0c04da63bdea82b0bb247d7497fbf88fcbfa6
Aaron Maywood <am7274183@gmail.com>
2022-03-16 14:51:50 +0900

    typo、及びhtml 上でコンテンツをわざとずらしてみた。どこまで耐久性があるか？

--- playwright.config.js
+++ playwright.config.js
@@ -20,7 +20,7 @@ const config = {
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 5000,
 	toMatchSnapshot: {
 	  maxDiffPixels: 100,
 	  threshold: 0.3,
